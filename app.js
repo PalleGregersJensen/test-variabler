5c7dd7d45b1487c1b08094dd354c837e0f92db0d
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-03-10 20:35:50 +0100

    Diverse øvelser fra slides s.60 og frem
    Der mangler fortsat at blive lavet lidt øvelser

---
+++ app.js
@@ -0,0 +1,29 @@
+"use strict"
+console.log("Javascript kører!")
+
+const bool = true;
+console.log("const bool = true");
+const num = 41;
+console.log("const num = 41");
+const str = "Peter";
+console.log("const str = Peter");
+const obj = {
+    cats: 2,
+    cars: 1
+};
+// console.log("const obj = 
+    // cats: 2,
+    // cars: 1
+// ");
+const nothing = null;
+console.log("const nothing = null");
+let undf; 
+console.log("let undf");
+const symbol = Symbol("symbol");
+console.log("const symbol = Symbol(symbol)");
+
+let fullName = "Alicia Keys";
+
+console.log(`hello ${1}`);
+console.log(`hello ${"name"}`);
+console.log(`hello ${fullName}`);
