5aae1af744b440a95e239c5fd18cf578afc81b58
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-03-11 12:59:38 +0100

    Øvelser med variabler og datatyper
    Brug evt som noter

--- app.js
+++ app.js
@@ -2,28 +2,43 @@
 console.log("Javascript kører!")
 
 const bool = true;
-console.log("const bool = true");
+console.log(bool);
 const num = 41;
-console.log("const num = 41");
+console.log(num);
 const str = "Peter";
-console.log("const str = Peter");
+console.log(str);
 const obj = {
     cats: 2,
     cars: 1
 };
-// console.log("const obj = 
-    // cats: 2,
-    // cars: 1
-// ");
+console.log(obj);
 const nothing = null;
-console.log("const nothing = null");
+console.log(nothing);
 let undf; 
-console.log("let undf");
+console.log(undf);
 const symbol = Symbol("symbol");
-console.log("const symbol = Symbol(symbol)");
+console.log(symbol);
 
 let fullName = "Alicia Keys";
 
 console.log(`hello ${1}`);
 console.log(`hello ${"name"}`);
 console.log(`hello ${fullName}`);
+
+
+// Omdan tekst til tal
+let string = "1234";
+console.log(Number(string));
+
+// Omdan tal til tekst
+let number = 1234;
+console.log(String(number));
+
+// Omdan tal til boolean - true
+let boolean = 1234;
+console.log(Boolean(boolean));
+
+console.log(Number("    123     "));
+console.log(Number("123z"));
+console.log(Number(true));
+console.log(Number(false));
